2032febb0f94c7bc676f2d56d760a51586288688
Russo-ow <e10benga@gmail.com>
2021-01-08 14:49:22 -0600

    Working on Weapon Pickup

---
+++ RomeroVision/Assets/Scripts/WeaponData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WeaponData : ScriptableObject {
+    new public string name;
+    public WeaponType type;
+    public float damage;
+    private float attackSpeed;
+
+    public Sprite sprite;
+
+    public float attackTime { get => 1/attackSpeed;}
+}
