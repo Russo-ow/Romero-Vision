19df16d213250b2581c4fb30ed9d1f326c32b3c7
Russo-ow <e10benga@gmail.com>
2021-01-08 14:10:10 -0600

    Player Movement

---
+++ RomeroVision/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D rb;
+
+    public float speed;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        rb.velocity = speed * new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")).normalized;
+    }
+}
